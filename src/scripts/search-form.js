a3a1ebc0111a91d7a00e87b66ace331942e8cd61
dev626 <jhonny.barrios@cfr-thesis.com>
2017-04-28 14:38:47 -0500

    full funcionality

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,21 +1,18 @@
 function searchForm(){
 	$('#textsearch').change(function(e) {
 		input = $(this);
-		console.log("input", input);
-	    console.log("input.val()", input.val());
-	    console.log("input.val().length", input.val().length);
     	if ( input.val().length > 2 ){
     		input.keypress(function(e) {
-    			if(e.which == 13) {
+    			if(e.which == 13 && input.val().length > 2 ) {
 			        search(input);
 			    }
     		});
+    		$('#searched').off();
 		    $('#searched').click( function(){
 		        search(input);
 		    });
     	} else {
     		$('#searched').off();
-    		input.off();
     	}
 	});
 }
@@ -23,18 +20,26 @@ function searchForm(){
 function search(input){
 	$('#result_search').fadeOut('slow', function() {
 		$('#result_search').html('');
+		list = new Array();
+		abc = new Array();
+		items = '';
 		$.getJSON( "books-schema.json", function( response ) {
-			var list = (response.data).map(function(i) { return i.title; });
-			var abc = list.filter(function(i) {
+			list = (response.data).map(function(i) { return i.title; });
+			abc = list.filter(function(i) {
 				a = i.toLowerCase();
 				b = input.val().toLowerCase();
 				if ( a.indexOf(b)  !== -1 ){
 					return '<a href="#">' + i + '</a>';
 				}
 			});
+			console.log("abc", abc);
 			$.each( abc, function( idx, val ) {
-				$('#result_search').append("<a href='#'>" + val + "</a>" );
+				if ( idx < 9 ){
+					console.log("idx, val", idx, val);
+					items += "<a href='#'>" + val + "</a>";					
+				}
 			});
+			$('#result_search').html(items);
 			$('#result_search').show();
 		});		
 	});;
