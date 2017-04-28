0e142ecfd5d315f732cb955bc14d93ecac9c5e25
Jhonny <jhonny25.bs@gmail.com>
2017-04-28 07:20:44 -0500

    maquetado

--- src/scripts/search-form.js
+++ src/scripts/search-form.js
@@ -1,3 +1,22 @@
 function searchForm(){
-    
+	$('#textsearch').change(function(e) {
+		input = $(this);
+    	if ( input.val().length > 2 ){
+    		input.keypress(function(e) {
+    			if(e.which == 13) {
+			        search(input.val());
+			    }
+    		});
+		    $('#searched').click( function(){
+		        search(input.val());
+		    });
+    	} else {
+    		$('#searched').off();
+    		input.off();
+    	}
+	});
+}
+
+function search(text){
+	console.log( text );
 }
