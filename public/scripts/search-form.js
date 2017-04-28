ad9de8334dd722435c15067d1c8dab10ae5b08ff
dev626 <jhonny.barrios@cfr-thesis.com>
2017-04-28 13:42:43 -0500

    first funcionality

--- public/scripts/search-form.js
+++ public/scripts/search-form.js
@@ -4,11 +4,11 @@ function searchForm(){
     	if ( input.val().length > 2 ){
     		input.keypress(function(e) {
     			if(e.which == 13) {
-			        search(input.val());
+			        search(input);
 			    }
     		});
 		    $('#searched').click( function(){
-		        search(input.val());
+		        search(input);
 		    });
     	} else {
     		$('#searched').off();
@@ -17,6 +17,49 @@ function searchForm(){
 	});
 }
 
-function search(text){
-	console.log( text );
+function search(input){
+	console.log( input.val() );
+	// var even = _.find([1, 2, 3, 4, 5, 6], function(num){ return num % 2 == 0; });
+
+	// console.log(even);
+
+	// $.getJSON( "books-schema.json", function( response ) {
+	// 	// console.log("data", response.data);
+
+	// 	// var even =  _.findWhere(response.data, {title: input.val()});
+		
+	// 	// console.log(even);
+
+
+	// // 	// var items = [];
+	// // 	// $.each( data, function( key, val ) {
+	// // 	// items.push( "<li id='" + key + "'>" + val + "</li>" );
+	// // 	// });
+
+	// // 	// $( "<ul/>", {
+	// // 	// "class": "my-new-list",
+	// // 	// html: items.join( "" )
+	// // 	// }).appendTo( "body" );
+	// });
+
+	// new Awesomplete(input, {
+	// 	minChars: 3,
+	// 	maxItems: 7,
+	// });
 }
+
+var ajax = new XMLHttpRequest();
+ajax.open("GET", "books-schema.json", true);
+ajax.onload = function( response) {
+	var json = JSON.parse(ajax.responseText);
+	console.log("json", json);
+	var list = (json.data).map(function(i) { return i.title; });
+	console.log("list", list);
+	new Awesomplete(document.querySelector("#textsearch"),{
+		minChars: 3,
+		maxItems: 7,
+		list: list
+	});
+};
+ajax.send();
+
